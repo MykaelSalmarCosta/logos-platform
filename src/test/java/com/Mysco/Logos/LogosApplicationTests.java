9b5d58e6a0d651ec85f9de27616aaf3b9d619557
MykaelSalmarCosta <devmysco@gmail.com>
2026-01-11 20:08:04 -0300

    iniciando projeto Logos

---
+++ src/test/java/com/Mysco/Logos/LogosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Mysco.Logos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LogosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
